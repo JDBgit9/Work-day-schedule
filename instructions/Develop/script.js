c9308365f0fbe29001c73d12b136aae17e1da4e9
JDBgit9 <info@jamesdbeeks.com>
2020-09-18 23:25:10 -0400

    js adds

--- instructions/Develop/script.js
+++ instructions/Develop/script.js
@@ -14,7 +14,47 @@ var nine = $
     // Change time black background color depending on the current our past, present, and future.
 console.log(moment().hour());
 
+
+
+
+
+
+
+
+
+
 hourRow.addClass("row hour form")
+hourCol.addClass("col-1")
+textArea.addClass("col-10")
+ServiceUIFrameContext.addClass("col-1 saveBtn")
+
+// give each hour row an attr. called "hour-data". with value equal to "hours[i]"
+textArea.attr("hour-data", parseInt(i + 9));
+saveText.attr("hour-data", parseIntl(i + 9));
+
+// add hour to variable to proper row
+hourCol.text(hours[i])
+
+// Add the Road to inner container then add each column to row
+$("innerContainer").append(hourRow);
+hourRow.append($(hourCol));
+hourRow.append($(textArea));
+hourRow.append($(saveText));
+saveText.text("");
+
+if (parseInt(textArea.attr("hour-data"))> currentHour) {
+     textArea.addClass("future")
+}
+
+if (textArea.attr("hour-data") === currentHour) {
+    textArea.addClass("present");
+}
+
+if (parseInt(textArea.attr("hour-data")) < currentHour) {
+    textArea.addClass("past");
+}
 
 
-turbo
+
+
+Turbo();
